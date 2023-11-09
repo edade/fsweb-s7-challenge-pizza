87e53b6386b07cad9cf9503835ad2ae418ce316b
Eda Kalaycioglu <edakalaycioglu@gmail.com>
2023-11-09 15:42:00 +0300

    Yup 3 aşama için kod yazıldı

--- src/components/OrderPizzaContent.js
+++ src/components/OrderPizzaContent.js
@@ -4,6 +4,7 @@ import Checkbox from "./Checkbox";
 import { useEffect } from "react";
 import OrderSum from "./OrderSum";
 import Note from "./Note";
+import * as Yup from 'yup';
 
 const OrderPizzaContent = () => {
   const [pizzaFiyat, setPizzaFiyat] = useState(85.5);
@@ -65,7 +66,39 @@ const OrderPizzaContent = () => {
     setToplamFiyat(yeniToplamFiyat);
     setSecimler(boyutEkFiyat + hamurEkFiyat + seciliMalzemeler.length * 5);
   }, [boyut, hamur, pizzaAdet, pizzaFiyat, seciliMalzemeler]);
+
+  const [formState, setFormState] = useState({
+    seciliMalzemeler: [],
+    boyut: "",
+    hamur: "",
+    pizzaAdet: 1
+  });
+
+  const [formErrors, setFormErrors] = useState({
+    seciliMalzemeler: "",
+    boyut: "",
+    hamur: "",
+    pizzaAdet: ""
+
+  });
+  const [formValid, setFormValid] = useState(true);
+
+  useEffect(() => {
+    productFormSchema
+        .isValid(formState)
+        .then(valid => setFormValid(valid) );
+      }, [formState]);
+
+
   
+
+  const productFormSchema = Yup.object().shape({
+    seciliMalzemeler: Yup.array().max(10, "En fazla 10 malzeme seçebilirsiniz.").min(3, "En az 3 malzeme seçebilirsiniz"),
+    boyut: Yup.string().required("Boyut seçimi yapmalısınız"),
+    hamur: Yup.string().required("Hamur seçimi yapmalısınız"),
+    pizzaAdet: Yup.number().min(1, "Adet eksi değer alamaz.")
+
+  });
   
 // Submite basılınca sayfanın rerender olmasını engellemek için preventDefault özelliğinden yararlanıyoruz
   function handleSubmit(e) {
